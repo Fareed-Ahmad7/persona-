eee97ee54f923e7bcb01c1113843d7b2e0851445
Fareed <90202062+Fareed-Ahmad7@users.noreply.github.com>
2022-12-06 19:56:54 +0530

    select image from gallery

---
+++ app/src/main/java/com/fareedahmad1097/persona/EditImage.java
@@ -0,0 +1,55 @@
+package com.fareedahmad1097.persona;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class EditImage extends AppCompatActivity {
+
+    // constant to compare
+    // the activity result code
+    int SELECT_PICTURE = 200;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_image);
+        imageChooser();
+    }
+
+    void imageChooser() {
+
+        // create an instance of the
+        // intent of the type image
+        Intent i = new Intent();
+        i.setType("image/*");
+        i.setAction(Intent.ACTION_GET_CONTENT);
+
+        // pass the constant to compare it
+        // with the returned requestCode
+        startActivityForResult(Intent.createChooser(i, "Select Picture"), SELECT_PICTURE);
+    }
+
+    // this function is triggered when user
+    // selects the image from the imageChooser
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK) {
+
+            // compare the resultCode with the
+            // SELECT_PICTURE constant
+            if (requestCode == SELECT_PICTURE) {
+                // Get the url of the image from data
+                Uri selectedImageUri = data.getData();
+                if (null != selectedImageUri) {
+                    ImageView image = findViewById(R.id.selectedImage);
+                    image.setImageURI(selectedImageUri);
+                }
+            }
+        }
+    }
+}
--- app/src/main/java/com/fareedahmad1097/persona/MainActivity.java
+++ app/src/main/java/com/fareedahmad1097/persona/MainActivity.java
@@ -1,14 +1,22 @@
 package com.fareedahmad1097.persona;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageButton button = findViewById(R.id.open_gallery);
+        button.setOnClickListener(v -> {
+            Intent myIntent = new Intent(MainActivity.this, EditImage.class);
+            MainActivity.this.startActivity(myIntent);
+        });
     }
 }
